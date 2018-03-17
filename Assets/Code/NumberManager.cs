c85534e45f898a3fcc7dc69d2a1973b9078194f9
deimors <deimors@gmail.com>
2018-03-17 16:16:07 -0600

    Simple Animations for Move and Merge

--- Assets/Code/NumberManager.cs
+++ Assets/Code/NumberManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -48,29 +49,58 @@ namespace Assets.Code
 				textComponent.fontSize = 24;
 		}
 
-		private void OnNumberMoved(NumberMovedEvent numberMovedEvent)
+		private async void OnNumberMoved(NumberMovedEvent numberMovedEvent)
 		{
 			var number = RemoveNumber(numberMovedEvent.Origin);
+
+			var originPosition = numberMovedEvent.Origin;
+			var targetPosition = numberMovedEvent.Target;
+
+			await AnimateMoveNumber(number, originPosition, targetPosition);
 
 			AddNumber(number, numberMovedEvent.Target);
 		}
 
-		private void OnNumbersMerged(NumbersMergedEvent numbersMergedEvent)
+		private static async Task AnimateMoveNumber(GameObject number, Position originPosition, Position targetPosition)
+		{
+			var origin = PositionToVector3(originPosition);
+			var toTarget = PositionToVector3(targetPosition) - origin;
+			var distanceToTarget = toTarget.magnitude;
+			var velocity = toTarget.normalized * 5;
+
+			var rectTransform = number.GetComponent<RectTransform>();
+			while ((rectTransform.localPosition - origin).magnitude < distanceToTarget)
+			{
+				rectTransform.localPosition += velocity;
+				await Task.Delay(TimeSpan.FromSeconds(0.01f));
+			}
+		}
+
+		private async void OnNumbersMerged(NumbersMergedEvent numbersMergedEvent)
 		{
 			var firstNumber = RemoveNumber(numbersMergedEvent.Origin1);
 			var secondNumber = RemoveNumber(numbersMergedEvent.Origin2);
+
+			await Task.WhenAll(
+				AnimateMoveNumber(firstNumber, numbersMergedEvent.Origin1, numbersMergedEvent.Target),
+				AnimateMoveNumber(secondNumber, numbersMergedEvent.Origin2, numbersMergedEvent.Target)
+			);
 
 			Destroy(secondNumber);
 
 			AddNumber(firstNumber, numbersMergedEvent.Target);
 			SetNumberText(firstNumber, numbersMergedEvent.NewNumber);
 		}
-
-
+		
 		private void AddNumber(GameObject number, Position position)
 		{
-			number.GetComponent<RectTransform>().localPosition = new Vector3(position.Column * 72, position.Row * -72);
+			number.GetComponent<RectTransform>().localPosition = PositionToVector3(position);
 			_numbers[position] = number;
+		}
+
+		private static Vector3 PositionToVector3(Position position)
+		{
+			return new Vector3(position.Column * 72, position.Row * -72);
 		}
 
 		private GameObject RemoveNumber(Position position)
